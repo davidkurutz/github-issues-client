60bf64ca66521752dfb5ee1b7816c04f9298fed5
Terry Lee <dmono@users.noreply.github.com>
2017-06-30 13:55:40 -0700

    added separate component for a single issue

---
+++ src/components/Issue.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row well">
+    <div class="col-md-6">
+      <a v-bind:href="issue.html_url">{{ issue.title }}</a>
+    </div>
+    <div class="col-md-2">
+      {{ issue.state }}
+    </div>
+    <div class="col-md-2">
+      {{ issue.created_at }}
+    </div>
+    <div class="col-md-2">
+      <a v-bind:href="issue.user.html_url">{{ issue.user.login }}</a>
+    </div>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    props: ['issue'],
+  };
+</script>
